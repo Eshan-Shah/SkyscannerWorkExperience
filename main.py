c6274fd9e102326652a9973dae6ea1b9c604daac
Eshan Shah <eshan.shah19@gmail.com>
2025-04-08 06:58:27 -0700

    Created the API

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
 from fastapi import FastAPI
+
+app = FastAPI()
+
+items_db = []
+
+@app.get("/form/")
+async def check(name: str='', email: str=''):
+
+    if name_empty(name):
+        return 'Please enter a valid name'
+    
+    elif email_empty(email):
+        return 'Please enter a valid email'
+    
+    elif email_exists(email):
+        return 'Email already exists'
+    
+    else:
+        items_db.append({"name": name, "email": email})
+        return 'Thanks'
+    
+@app.get("/items/")
+async def get_items():
+    return items_db
+
+def name_empty(name):
+    if name == '' or name == None:
+        return True
+    return False
+
+def email_empty(email):
+    if email == '' or email == None:
+        return True
+    return False
+            
+def email_exists(email):
+    for item in items_db:
+        if email == item["email"]:
+            return True
+        
+    return False
---
+++ users.js
@@ -0,0 +1,4 @@
+const input = document.getElementById("name");
+const inputValue = input.value;
+console.log(inputValue);
+
